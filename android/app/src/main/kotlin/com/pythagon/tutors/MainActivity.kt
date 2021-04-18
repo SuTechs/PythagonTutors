855ee97461e53364e15b5906456359f2cd2bc4ad
SuTechs <sumit123210@gmail.com>
2021-04-18 19:43:02 +0530

    Initial project

---
+++ android/app/src/main/kotlin/com/pythagon/tutors/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pythagon.tutors
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pythagon Tutors',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('HELLO'),
+        ),
+      ),
+    );
+  }
+}
