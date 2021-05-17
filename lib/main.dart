241033bbc95bc780cd97208fd8a966592d136d52
SuTechs <sumit123210@gmail.com>
2021-05-17 14:13:03 +0530

    attachments button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:get/get.dart';
 
 import '/constants.dart';
@@ -8,8 +9,8 @@ import '/screens/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp();
+  await FlutterDownloader.initialize();
 
   runApp(MyApp());
 }
--- lib/widgets/assignmentListTile.dart
+++ lib/widgets/assignmentListTile.dart
@@ -1,9 +1,16 @@
+import 'dart:io';
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:get/get.dart';
 import 'package:get/utils.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:tutors/data/database.dart';
 import 'package:tutors/data/utils/Utils.dart';
+import 'package:tutors/widgets/fileIcons.dart';
 
 import '../constants.dart';
 
@@ -130,20 +137,6 @@ class AssignmentDetail extends StatelessWidget {
         ),
       );
 
-  Widget getAttachment() => Container(
-        margin: EdgeInsets.only(right: 12),
-        width: 46.0,
-        height: 46.0,
-        decoration: BoxDecoration(
-          color: kBlueColor,
-          borderRadius: BorderRadius.circular(6.0),
-          image: DecorationImage(
-            image: const AssetImage('assets/icons/google.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-      );
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -188,9 +181,15 @@ class AssignmentDetail extends StatelessWidget {
             getTitle('Attachments', context),
             Row(
               children: [
-                getAttachment(),
-                getAttachment(),
-                getAttachment(),
+                for (String url in assignment.assignmentData.referenceFiles)
+                  AttachmentButton(url: url),
+
+                /// test
+                for (Map<String, String> s in _images)
+                  AttachmentButton(
+                    url: s['link']!,
+                    key: UniqueKey(),
+                  ),
               ],
             ),
 
@@ -302,6 +301,212 @@ enum AssignmentStatus {
   Completed,
 }
 
+/// attachments buttons
+
+class AttachmentButton extends StatefulWidget {
+  final String url;
+
+  const AttachmentButton({Key? key, required this.url}) : super(key: key);
+
+  @override
+  _AttachmentButtonState createState() => _AttachmentButtonState();
+}
+
+class _AttachmentButtonState extends State<AttachmentButton> {
+  bool isDownloading = false;
+  late String _taskId = 'hello';
+  // ReceivePort _port = ReceivePort();
+  // double downloadProgress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // _bindBackgroundIsolate();
+    FlutterDownloader.registerCallback(downloadCallback);
+  }
+
+  // @override
+  // void dispose() {
+  //   _unbindBackgroundIsolate();
+  //   super.dispose();
+  // }
+
+  // void _bindBackgroundIsolate() {
+  //   bool isSuccess = IsolateNameServer.registerPortWithName(
+  //       _port.sendPort, 'downloader_send_port');
+  //   if (!isSuccess) {
+  //     _unbindBackgroundIsolate();
+  //     _bindBackgroundIsolate();
+  //     return;
+  //   }
+  //   _port.listen((dynamic data) {
+  //     // print('UI Isolate Callback: $data');
+  //
+  //     String id = data[0];
+  //     DownloadTaskStatus status = data[1];
+  //     int progress = data[2];
+  //
+  //     print('Su taskId = $_taskId and id = $id');
+  //
+  //     if (_taskId != 'hello' && id == _taskId) {
+  //       print('hello SuMit');
+  //
+  //       setState(() {
+  //         if (status == DownloadTaskStatus.running)
+  //           downloadProgress = progress / 100;
+  //
+  //         if (status == DownloadTaskStatus.complete ||
+  //             status == DownloadTaskStatus.failed) isDownloading = false;
+  //       });
+  //
+  //       if (status == DownloadTaskStatus.complete)
+  //         FlutterDownloader.open(taskId: _taskId);
+  //     }
+  //
+  //     // if (_tasks != null && _tasks!.isNotEmpty) {
+  //     //   final task = _tasks!.firstWhere((task) => task.taskId == id);
+  //     //   if (task != null) {
+  //     //     setState(() {
+  //     //       task.status = status;
+  //     //       task.progress = progress;
+  //     //     });
+  //     //   }
+  //     // }
+  //   });
+  // }
+
+  // void _unbindBackgroundIsolate() {
+  //   IsolateNameServer.removePortNameMapping('downloader_send_port');
+  // }
+
+  static void downloadCallback(
+      String id, DownloadTaskStatus status, int progress) {
+    // final SendPort send =
+    //     IsolateNameServer.lookupPortByName('downloader_send_port')!;
+    // send.send([id, status, progress]);
+
+    // print('HEllO SuMIT');
+    // if (status == DownloadTaskStatus.failed)
+    //   showError('Failed!', 'The download could not be completed.');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 12),
+      decoration: isDownloading
+          ? null
+          : BoxDecoration(
+              borderRadius: BorderRadius.circular(6.0),
+              border: Border.all(color: kBlueColor, width: 0.2),
+            ),
+      child: isDownloading
+          ? Stack(
+              children: [
+                CircleAvatar(
+                  radius: 22,
+                  backgroundColor: Colors.transparent,
+                  child: FileIcon(
+                    widget.url.split('/').last,
+                    size: 40,
+                  ),
+                ),
+                SizedBox(
+                  height: 44,
+                  width: 44,
+                  child: CircularProgressIndicator(
+                    // value: downloadProgress,
+                    strokeWidth: 1,
+                  ),
+                ),
+              ],
+            )
+          : GestureDetector(
+              child: FileIcon(
+                widget.url.split('/').last,
+                size: 46,
+              ),
+              onTap: () async {
+                if (_taskId != 'hello') {
+                  final b = await FlutterDownloader.open(taskId: _taskId);
+                  if (b) return;
+                }
+
+                Get.snackbar(
+                    "Downloading!", 'Chek notification for more details.');
+
+                setState(() {
+                  isDownloading = true;
+                });
+
+                await onFileClick().catchError((e) {
+                  printError(info: 'Error #2325 = $e');
+
+                  showError("Error #2325",
+                      'Something went wrong. We will try to fix it ASAP!');
+                });
+
+                await Future.delayed(Duration(milliseconds: 2000));
+
+                setState(() {
+                  isDownloading = false;
+                });
+              },
+            ),
+    );
+  }
+
+  static void showError(String title, String message) {
+    Get.snackbar(
+      title,
+      message,
+      backgroundColor: Colors.red,
+      colorText: Colors.white,
+    );
+  }
+
+  Future<void> onFileClick() async {
+    /// chek for permission
+    final status = await Permission.storage.status;
+    if (status != PermissionStatus.granted) {
+      final result = await Permission.storage.request();
+      if (result != PermissionStatus.granted) {
+        showError(
+          'Permission Denied!',
+          'Storage permission is require to download files',
+        );
+        return;
+      }
+    }
+
+    /// getting path
+    final directory = await getExternalStorageDirectory();
+    print('Directory = $directory');
+    if (directory == null) {
+      showError('Error!', 'No directory to download file.');
+      return;
+    }
+    final localPath = directory.path + Platform.pathSeparator + 'Download';
+    final savedDir = Directory(localPath);
+    bool hasExisted = await savedDir.exists();
+    if (!hasExisted) {
+      savedDir.create();
+    }
+
+    /// downloading files
+
+    final t = await FlutterDownloader.enqueue(
+        url: widget.url,
+        savedDir: localPath,
+        showNotification: true,
+        openFileFromNotification: true);
+
+    if (t != null) _taskId = t;
+
+    print('Task Id = $_taskId');
+  }
+}
+
 // const _kButtonStatusColors = {
 //   AssignmentStatus.Pending: kBlueColor,
 //   AssignmentStatus.NotAssigned: Colors.red,
@@ -334,3 +539,26 @@ const _kButtonStatusIcons = {
   AssignmentStatus.Uploaded: Icons.access_time_rounded,
   AssignmentStatus.Completed: Icons.done,
 };
+
+const _images = [
+  // {
+  //   'name': 'Arches National Park',
+  //   'link':
+  //       'https://upload.wikimedia.org/wikipedia/commons/6/60/The_Organ_at_Arches_National_Park_Utah_Corrected.jpg'
+  // },
+  // {
+  //   'name': 'Canyonlands National Park',
+  //   'link':
+  //       'https://upload.wikimedia.org/wikipedia/commons/7/78/Canyonlands_National_Park%E2%80%A6Needles_area_%286294480744%29.jpg'
+  // },
+  // {
+  //   'name': 'Death Valley National Park',
+  //   'link':
+  //       'https://upload.wikimedia.org/wikipedia/commons/b/b2/Sand_Dunes_in_Death_Valley_National_Park.jpg'
+  // },
+  {
+    'name': 'Gates of the Arctic National Park and Preserve',
+    'link':
+        'https://upload.wikimedia.org/wikipedia/commons/e/e4/GatesofArctic.jpg'
+  }
+];
